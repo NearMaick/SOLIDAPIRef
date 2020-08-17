a744b588ba381f381b59476f6243c244eb93d40e
Maick Souza <maick_a_s@msn.com>
2020-08-17 16:52:41 -0300

    chore(server): SOLID #2

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,18 @@
+import { FakeUsersRepository } from "../../repositories/implementations/FakeUsersRepository";
+import { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserController } from "./CreateUserController";
+
+const fakeUsersRepository = new FakeUsersRepository();
+const mailTrapMailProvider = new MailtrapMailProvider();
+
+const createUserUseCase = new CreateUserUseCase(
+  fakeUsersRepository,
+  mailTrapMailProvider,
+);
+
+const createUserController = new CreateUserController(
+  createUserUseCase,
+);
+
+export { createUserUseCase, createUserController };
